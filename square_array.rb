00b72062bccf642b1060c5ceeeb3104ec447769a
Merline Belizaire <merline@noukod.com>
2018-09-21 14:14:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+arrays = []
+counter = 1
+array.each do |element|
+  puts "#{counter}"
+  puts " #{element}"
+  counter += 1
+  arrays.push element * element
+end
+arrays
 end
