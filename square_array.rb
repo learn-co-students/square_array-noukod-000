60f23977dfe7ebdf88dee6e23ba65ec0235cffd1
Saint-Jean Germila <germila@noukod.com>
2018-09-24 14:20:47 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers = [1,2,3]
+  numbers.map! { |a| a*2 }
+  a.each_index { |a| print a,--- }
+ numbers.collect!.with_index { |x, i| x[0...i] }
+
 end
